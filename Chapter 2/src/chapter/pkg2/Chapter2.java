35b658a00b4c09f1fd9b4352a738e4785aeb4332
dmis32 <70528749+dmis32@users.noreply.github.com>
2020-09-14 10:36:10 -0400

    change

--- Chapter 2/src/chapter/pkg2/Chapter2.java
+++ Chapter 2/src/chapter/pkg2/Chapter2.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chapter.pkg2;
 
-/**
- *
- * @author Dylan
- */
 public class Chapter2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Happy Tuesday");
+        // if you use print instead of println, it will be all on one line
+        System.out.print("This is just a print statement\n");
+        System.out.print("Another Print\n");
+        System.out.println("programmin is \"fun\"");
+        System.out.println("welcome to our java shop");
+        System.out.println("\tl. large coffee\t\t$1.00");
+        System.out.println("\t2. espresso shot\t\t$0.50");
+        
+        String menu - "Welcome to our java shop\n"+
+        
+        
+ 
+              
     }
     
 }
