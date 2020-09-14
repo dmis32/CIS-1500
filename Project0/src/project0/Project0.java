7d9f406f280ea9500190a45d26fdb1eb794a213d
dmis32 <70528749+dmis32@users.noreply.github.com>
2020-09-14 13:42:33 -0400

    final changes

--- Project0/src/project0/Project0.java
+++ Project0/src/project0/Project0.java
@@ -11,7 +11,25 @@ public class Project0 {
         Scanner keyboard = new Scanner(System.in);
         name = keyboard.nextLine();
         
-        System.out.println("My name is " + name + ". nice to meet you" );
+        System.out.println("Nice to meet you. " + name + ", How many credits have you earned already?" );
+        int numberofcredits = keyboard.nextInt();
+                keyboard.nextLine();
+                
+        System.out.println("Dylan,how many credits do you need to earn your bachelors degree?");
+        int numberofcreditsleft = keyboard.nextInt();
+                keyboard.nextLine();
+                
+        System.out.println("Dylan, how many credits do you take each semester?");
+        int numberofsemestercredits = keyboard.nextInt();
+        keyboard.nextLine();
+                
+        System.out.println("Dylan, how much does 1 credit hour cost?");
+        int costofcredit = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        System.out.println("calculating...");
+        System.out.println("Dylan, based on my calculations,it will take you 6 more semesters and a total cost of $43,380 plus additional fees");
+        
     }
     
 }
