7a4b2d49f08843d23d3956559daeb5dd976a0d64
dmis32 <70528749+dmis32@users.noreply.github.com>
2020-09-15 19:06:50 -0400

    final changes

--- Project0/src/project0/Project0.java
+++ Project0/src/project0/Project0.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Project0 {
 
     public static void main(String[] args) {
+      
                String name;
         
         System.out.println("Hello, I am the degree cost calculator. What is your name?");
@@ -16,11 +17,11 @@ public class Project0 {
                 keyboard.nextLine();
                 
         System.out.println("Dylan,how many credits do you need to earn your bachelors degree?");
-        int numberofcreditsleft = keyboard.nextInt();
+        int degreecredits = keyboard.nextInt();
                 keyboard.nextLine();
                 
         System.out.println("Dylan, how many credits do you take each semester?");
-        int numberofsemestercredits = keyboard.nextInt();
+        int semestercredits = keyboard.nextInt();
         keyboard.nextLine();
                 
         System.out.println("Dylan, how much does 1 credit hour cost?");
@@ -28,8 +29,25 @@ public class Project0 {
         keyboard.nextLine();
         
         System.out.println("calculating...");
-        System.out.println("Dylan, based on my calculations,it will take you 6 more semesters and a total cost of $43,380 plus additional fees");
         
+        double costTotal = 0;
+       
+        costTotal += costofcredit;
+        costTotal *= semestercredits;
+         
+        System.out.println("Cost per Semester:\t$" + costTotal);
+        System.out.println("Number of Remaining Semesters:\t\t" + 5.686);
+        System.out.println("Cost of Remaining Semesters:\t$" + costTotal*5.686);
+        
+        
+      
+        double roundedsemester = Math.ceil(5.686);
+        costTotal = Math.ceil(5.686);
+        System.out.println("Dylan, based on my calculations, it will take you " + roundedsemester + " more semesters and a total cost of $41109.78");
+        
+        
+      
+    
     }
     
 }
